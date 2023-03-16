a77af08bf647c2a10d14431d7add55220810d5f1
pushpa <nelluripushpa1991@gmail.com>
2023-03-16 16:25:12 -0600

    Intitial commit for curd operations

---
+++ src/main/java/com/pushpa/springboot/security/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.pushpa.springboot.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @RequestMapping("/")
+    public String welcome () {
+        return "Welcome To Spring Boot Security";
+    }
+}
